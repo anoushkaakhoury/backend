8ee0d07521df9a48beb33e9c4ecf55e763cd8636
anoushkaakhoury <anoushkaakhoury@gmail.com>
2021-02-25 18:23:02 +0530

    new

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd ..
 #npm --prefix /home/ubuntu/app.js start
-sudo pm2 restart /home/ubuntu/app.js
+sudo pm2 restart /home/ubuntu/node-express-realworld-example-app/app.js
 #npm  --prefix /home/ubuntu/app.js start
